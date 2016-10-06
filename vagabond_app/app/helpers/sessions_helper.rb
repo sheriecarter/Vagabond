8392ee296b647e6226179874c8f4a78f032b8704
14budig <nbudig@gmail.com>
2016-10-06 16:37:12 -0700

    some bugfixing

--- vagabond_app/app/helpers/sessions_helper.rb
+++ vagabond_app/app/helpers/sessions_helper.rb
@@ -1,4 +1,4 @@
-module SessionHelper
+module SessionsHelper
 
   def login(user)
     session[:user_id] = user.id
@@ -18,5 +18,5 @@ module SessionHelper
   def logout
     @current_user = session[:user_id] = nil
   end
-  
+
 end
