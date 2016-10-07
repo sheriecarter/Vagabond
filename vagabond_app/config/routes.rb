0576ad58cace9296f3bf88e1a96560e769b98190
Scott Whitman <scott.w.whitman@gmail.com>
2016-10-06 22:56:58 -0700

    added posts model routes controller views

--- vagabond_app/config/routes.rb
+++ vagabond_app/config/routes.rb
@@ -13,4 +13,9 @@ Rails.application.routes.draw do
   get "/users/:id", to:"users#show", as: "user"
   post "/users", to: "users#create"
 
+  get "/posts", to: "posts#index"
+  get "/posts/new", to: "posts#new", as: "new_post"
+  get "/posts/:id", to: "posts#show", as: "post"
+  post "/posts", to: "posts#create"
+
 end
