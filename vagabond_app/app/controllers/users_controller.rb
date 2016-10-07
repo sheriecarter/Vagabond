82b0bb52711c8d93ad892349a6c844397d4ea25a
Andrew Cordivari <andrewcord@William-Cordivaris-MacBook-Pro.local>
2016-10-06 17:02:46 -0700

    post, new, create routes work for user

--- vagabond_app/app/controllers/users_controller.rb
+++ vagabond_app/app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to user_path(@user)
+    end
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :current_city, :email, :password)
+  end
+
 end
--- vagabond_app/config/routes.rb
+++ vagabond_app/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: "users#index"
 
-
-
+get "/users", to: "users#index"
+get "/users/new", to: "users#new", as: "new_user"
+get "/users/:id", to:"users#show", as: "user"
+post "/users", to: "users#create"
 
 end
