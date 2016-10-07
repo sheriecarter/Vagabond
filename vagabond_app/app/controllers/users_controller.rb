ed91d270d4741a5d5c2d9bb1bfa3fbfd6a31fe1a
Andrew Cordivari <andrewcord@williamvarismbp.gateway.sonic.net>
2016-10-06 22:13:09 -0700

    refactored create method in users controller

--- vagabond_app/app/controllers/users_controller.rb
+++ vagabond_app/app/controllers/users_controller.rb
@@ -9,10 +9,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to user_path(@user)
-    end
+    @user = User.create(user_params)
+    login(@user)
+      redirect_to @user
   end
 
   def show
@@ -29,7 +28,7 @@ class UsersController < ApplicationController
     user = User.find_by_id(user_id)
     user.update_attributes(user_params)
     redirect_to user_path(user)
-  
+
   end
 
 
