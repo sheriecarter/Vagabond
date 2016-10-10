f89f6130060cad20451639aa75f0c9f2a24ed472
Sherie Carter <sherie1.carter@gmail.com>
2016-10-09 21:50:31 -0700

    user and post validations

--- vagabond_app/app/models/post.rb
+++ vagabond_app/app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :city, optional: true
 
+  validates :title, length: {in:1..200}
+  validates :body, length: {minimun: 1}
 end
