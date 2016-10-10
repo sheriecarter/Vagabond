805e2838627b890f159391c515515b021265d11f
14budig <nbudig@gmail.com>
2016-10-10 12:31:10 -0700

    more bugs

--- vagabond_app/config/routes.rb
+++ vagabond_app/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'errors/not_found'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "users#index"
+  root to: "cities#index"
 
   get "/login", to: "sessions#new"
   post "/sessions", to: "sessions#create"
