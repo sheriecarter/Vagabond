c600235c76ee6370d09d1da69194a569af656f87
14budig <nbudig@gmail.com>
2016-10-12 11:41:13 -0700

    pre-deploy to heroku

--- vagabond_app/app/helpers/cities_helper.rb
+++
@@ -1,5 +0,0 @@
-module CitiesHelper
-  def current_city
-    @current_city ||= City.find_by_id[:city_id]
-  end
-end
--- vagabond_app/app/models/post.rb
+++
@@ -1,7 +0,0 @@
-class Post < ApplicationRecord
-  belongs_to :user, optional: true
-  belongs_to :city, optional: true
-
-  validates :title, length: { in:1..200 }
-  validates :body, length: { minimum: 1 }
-end
